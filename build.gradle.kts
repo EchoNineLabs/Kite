79199463596086f9d76f9d01a824198dad0042b4
Michał <michal.czopek.foss@proton.me>
2026-05-06 12:04:39 +0200

    1.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ private val SUPPORTED_VERSIONS = listOf(
 )
 
 group = "dev.echonine.kite"
-version = "1.4.3"
+version = "1.4.4"
 
 if (System.getenv("CI") != "true") {
     val commitHash = ProcessBuilder(listOf("git", "rev-parse", "--short", "--verify", "HEAD"))

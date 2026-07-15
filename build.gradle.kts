9cd609a8818963e556518d2de5d3aec50c127355
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2026-07-15 11:45:38 +0200

    1.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ private val SUPPORTED_VERSIONS = listOf(
 )
 
 group = "dev.echonine.kite"
-version = "1.6.1"
+version = "1.6.2"
 
 if (System.getenv("CI") != "true") {
     val commitHash = ProcessBuilder(listOf("git", "rev-parse", "--short", "--verify", "HEAD"))
